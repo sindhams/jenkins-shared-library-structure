0e2be848d567e6f96253b5796939288bd0edd533
sindhams <31239460+sindhams@users.noreply.github.com>
2020-11-11 19:34:35 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -4,8 +4,6 @@ def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]],
-        changelog: [[ stageParams.changelog ]],
-        poll: [[ stageParams.poll ]]
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
   }
